49952443d31eb661fbf56febb1740f2e5ea148b0
sunwan1234 <69964957+sunwan1234@users.noreply.github.com>
2020-08-27 21:33:46 +0800

    Add files via upload

---
+++ dist/precache-manifest.fc2bcd5b6569fb3d94ef92511583d0b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740c3738420f7d8bdc0c",
+    "url": "/apple-bill/css/app.1ee5b28c.css"
+  },
+  {
+    "revision": "2d19ca1f22a15e67bd282bc9b2835e6c",
+    "url": "/apple-bill/index.html"
+  },
+  {
+    "revision": "740c3738420f7d8bdc0c",
+    "url": "/apple-bill/js/app.6725149d.js"
+  },
+  {
+    "revision": "156623125508d76d2a43",
+    "url": "/apple-bill/js/chunk-vendors.2c2d4054.js"
+  },
+  {
+    "revision": "2342ddc195ea9ca0007f3aedc8bed83f",
+    "url": "/apple-bill/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/apple-bill/robots.txt"
+  }
+]);
